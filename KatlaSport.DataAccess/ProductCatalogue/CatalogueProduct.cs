ed5c8fce660f10ed2fd785ada88b4309289c4274
Domino <zhikhanastasya@gmail.com>
2018-10-04 01:01:34 +0300

    Merge step3. Add ProductCount to ProductCategoryListItem model. Get ProductCount evaluation logic to ProductCategoryService.GetCategoriesAsync.

--- KatlaSport.DataAccess/ProductCatalogue/CatalogueProduct.cs
+++ KatlaSport.DataAccess/ProductCatalogue/CatalogueProduct.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using KatlaSport.DataAccess.ProductStore;
 
 namespace KatlaSport.DataAccess.ProductCatalogue
